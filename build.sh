feaa719e73d90f8df194edb5a0faf684f8ee5144
jamie <jmleary@willamette.edu>
2018-02-12 19:48:30 -0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 cd writing
+dir="writing/"
 
 for i in *.md
 do
     pandoc "$i" -o ${i%.*}.pdf
-    echo "generated ${i%.*}.pdf"
+    echo "generated ${dir}${i%.*}.pdf"
 done
+
+cd ..
